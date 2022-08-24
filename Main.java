c8a9c1d1b85ce2be2194c39c9756eab3ead4bd7b
Andruss <brennana46274@stu.powayusd.com>
2022-08-24 14:43:32 -0700

    changes

--- Main.java
+++ Main.java
@@ -19,10 +19,32 @@ class Main
             index = rand.nextInt(wordList.length);
             words += wordList[index] + " ";
         }
+        words.trim();
 
+        // Print words and get user input
         System.out.println("Type the words below: ");
         System.out.println(words);
         wordsInput = input.nextLine();
+
+        // Determine and print results
+        int wordsLength = 0;
+        int var1 = words.length();
+        int var2 = wordsInput.length();
+        if(var1 > var2)
+        {
+            System.out.println("y");
+        }
+        int correctChars = 0;
+        for(int i = 0; i < wordsLength; i++)
+        {
+            if(words.charAt(i) == wordsInput.charAt(i))
+            {
+                correctChars++;
+            }
+        }
+        
+        //
         System.out.println(wordsInput);
+        System.out.println(correctChars);
     }
 }
