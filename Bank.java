abeb6f581ac9843cad5195860ff72a8d988bf5cf
Andruss <brennana46274@stu.powayusd.com>
2022-08-25 14:25:10 -0700

    changes

--- Bank.java
+++ Bank.java
@@ -1,6 +1,6 @@
 public class Bank {
     public static String[] wordBank = {
         "person", "place", "thing", "and", "an", "any", "the", "them", "they", "one", "if", "in", "a", "is", "it", 
-        "there", "people", "nation", "number", "about"
+        "there", "people", "nation", "number", "about", "use", "or", "for", "end", "now", "few", "no", "new", "now"
     };
 }
